5c59d5c7a06fa5901c4c904d8867d2cf4f81e5d6
Juan Garcia <juand.garciap.2006@gmail.com>
2024-12-06 11:47:25 -0400

    feat: ✨ scrip responsive

--- scrip.js
+++ scrip.js
@@ -25,12 +25,18 @@
 
 
 const createSongComponent = (song) => {
+
+    // creo un elemento div
     const div = document.createElement('div')
+
+    // establezco que atributos tendra el div
     div.setAttribute('class', 'canciones')
+
+    // crea el contenido html para la etiqueta div
     div.innerHTML = `
                       <img class="canciones__img" src="${song.image.url} " alt="">
                 <div class="canciones__body">
-                    <h3 class="canciones__body__title">${song.title} </h3>
+                    <h3 class="canciones__body__title">${song.title}</h3>
                     <h4 class="canciones__body__autor">${song.author} </h4>
                 </div>
                 <div class="canciones__footer">
@@ -45,18 +51,44 @@ const createSongComponent = (song) => {
                     </div>
                 </div>
     `
+
+    setInteractiveComponent(div, song)
     return div
 } 
 
+const setInteractiveComponent = (component, song) => {
+    component.addEventListener("click", () => { 
+        document.getElementById("audio").setAttribute("src", song.audio.url)
+        document.getElementById("imagen").setAttribute("src", song.image.url)
+        document.getElementById("title").innerHTML = ("src", song.title)
+        // document.getElementById("background").innerHTML = ("src", song.image.url)
+        document.getElementById("imgReproductor").style.backgroundImage = `url(${song.image.url})`
+    })
+}
+document.getElementById('play').addEventListener('click', () => {
+    const audio = document.getElementById('audio')
 
+    if (audio.paused) {
+        audio.play()
+        document.getElementById('play').setAttribute('src', '/assets/play.svg')
+    } else {
+        audio.pause()
+    }
+})
+
+// las funciones tipo flecha solo lo mismo pero mejor
 axios.get('https://api.institutoalfa.org/api/songs')
 .then(function (response) {
-  const songsInfo = response.data.songs
-  const contenedor = document.getElementById('container-song')
+    const songsInfo = response.data.songs
+    const contenedor = document.getElementById('container-song')
 
-  songsInfo.map((song) => {
-        const songComponent = createSongComponent(song)
-        contenedor.appendChild(songComponent)
+// el .map es un bucle
+    songsInfo.map((song) => {
+          const songComponent = createSongComponent(song)
+
+        //   le da a contenedor un elemento hijo
+          contenedor.appendChild(songComponent)
+
 
     })
   })
